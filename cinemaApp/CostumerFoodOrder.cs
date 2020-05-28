bdc3f9b9a6a756c561a7015e4a8ab20ad9dbb751
0989115 <55788313+0989115@users.noreply.github.com>
2020-05-28 06:42:08 +0200

    Update CostumerFoodOrder.cs

--- cinemaApp/CostumerFoodOrder.cs
+++ cinemaApp/CostumerFoodOrder.cs
@@ -1,16 +1,15 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace Cinema_
+namespace CinemaApp
 {
     class CostumerFoodOrder
     {
 
         public List<FoodOrder> Orders;
-
 
         public CostumerFoodOrder(List<FoodOrder> orders)
         {
@@ -40,6 +39,7 @@ namespace Cinema_
             foreach (var order in userOrders)
             {
                 order.Paid = true;
+                order.displayTime();
                 order.Order.payOverview();
             }
 
@@ -93,26 +93,44 @@ namespace Cinema_
                 }
             }
 
-            Console.WriteLine("In your cart");
+            Console.WriteLine("\nIn your cart");
             foreach (var o in userOrders)
             {
-                o.Paid = true;
+                o.displayTime();
                 o.Order.payOverview();
             }
+           
         }
 
-        public void Costumer()
+        public void Remove()
+        {
+            string username = "default";
+            List<FoodOrder> userOrders = new List<FoodOrder>();
+            foreach (var order in Orders)
+            {
+                if (order.UserName == username)
+                {
+                    userOrders.Add(order);
+                }
+            }
+
+
+        }
+
+        public static void Costumer()
         {
             bool busy = true;
-            string fileName = "food.json";
+            var TheMenu = FoodMenu.getMenu();
+
+            string fileName = "allOrders.json";
             string rawJson = File.ReadAllText(fileName);
-            List<Food> menu = JsonConvert.DeserializeObject<List<Food>>(rawJson);
-            var TheMenu = new FoodMenu(menu);
+            List<FoodOrder> all = JsonConvert.DeserializeObject<List<FoodOrder>>(rawJson);
+            var allOrders = new CostumerFoodOrder(all);
             
 
             while (busy)
             {
-                Console.WriteLine("\nHi, what do you wanna do?\n1. See menu\n2. Search menu\n3. See what's in your cart\n4. Add something to cart\n5. Checkout\npress q to quit....");
+                Console.WriteLine("\nHi, what do you wanna do?\n1. See menu\n2. Search menu\n3. See what's in your cart\n4. New food order\n5. Checkout\npress q to quit....");
                 var choice = Console.ReadKey().Key;
                 if (choice == ConsoleKey.Q)
                 {
@@ -129,24 +147,21 @@ namespace Cinema_
                 }
                 else if (choice == ConsoleKey.D3)
                 {
-                    seeCart();
+                    allOrders.seeCart();
                 }
                 else if (choice == ConsoleKey.D4)
                 {
-                    //addFoodOrder();
-                    string fName = "allOrders.json";
-                    string rJson = File.ReadAllText(fName);
-                    List<FoodOrder> all = JsonConvert.DeserializeObject<List<FoodOrder>>(rJson);
                     all.Add(FoodOrder.Add());
-                    CostumerFoodOrder allOrders = new CostumerFoodOrder(all);
-                    string newJson = JsonConvert.SerializeObject(all);
-                    File.WriteAllText(fName, newJson);
+                    allOrders = new CostumerFoodOrder(all);
+                    
                 }
                 else if (choice == ConsoleKey.D5)
                 {
-                    checkout();
+                    allOrders.checkout();
                 }
             }
+            string newJson = JsonConvert.SerializeObject(all);
+            File.WriteAllText(fileName, newJson);
         }
     }
 }
