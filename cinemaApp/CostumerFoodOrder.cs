c47c0cfa863ab4ce93cc8e4f9b39e1a5d1e88f10
0989115 <55788313+0989115@users.noreply.github.com>
2020-05-28 17:59:23 +0200

    Update CostumerFoodOrder.cs

--- cinemaApp/CostumerFoodOrder.cs
+++ cinemaApp/CostumerFoodOrder.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace CinemaApp
 {
-    class CostumerFoodOrder
+        class CostumerFoodOrder
     {
 
         public List<FoodOrder> Orders;
@@ -21,7 +21,7 @@ namespace CinemaApp
             this.Orders = new List<FoodOrder>();
         }
 
-        public  void checkout()
+        public void checkout()
         {
             //collect orders of username
             string username = "default";
@@ -34,7 +34,7 @@ namespace CinemaApp
                 }
             }
 
-        
+
             Console.WriteLine("In your cart");
             foreach (var order in userOrders)
             {
@@ -45,25 +45,25 @@ namespace CinemaApp
 
             Console.WriteLine("Choose your bank:\n1. ABN AMRO\n2. ING\n3. Rabobank\n4. Paypal");
             var bank = Console.ReadKey().Key;
-            if(bank == ConsoleKey.D1)
+            if (bank == ConsoleKey.D1)
             {
                 Console.Write("NL02 ABNA ");
                 Console.ReadLine();
                 Console.WriteLine("Confirming purchase...");
             }
-            else if( bank == ConsoleKey.D2)
+            else if (bank == ConsoleKey.D2)
             {
                 Console.Write("NL38 INGB ");
                 Console.ReadLine();
                 Console.WriteLine("Confirming purchase...");
             }
-            else if(bank == ConsoleKey.D3)
+            else if (bank == ConsoleKey.D3)
             {
                 Console.Write("NL30 RABO ");
                 Console.ReadLine();
                 Console.WriteLine("Confirming purchase...");
             }
-            else if(bank == ConsoleKey.D4)
+            else if (bank == ConsoleKey.D4)
             {
                 Console.WriteLine("Email: ");
                 Console.ReadLine();
@@ -77,11 +77,11 @@ namespace CinemaApp
                     order.Paid = false;
                 }
             }
-            
+
 
         }
 
-        public  void seeCart()
+        public void seeCart()
         {
             string username = "default";
             List<FoodOrder> userOrders = new List<FoodOrder>();
@@ -94,12 +94,13 @@ namespace CinemaApp
             }
 
             Console.WriteLine("\nIn your cart");
-            foreach (var o in userOrders)
+            foreach (var fo in userOrders)
             {
-                o.displayTime();
-                o.Order.payOverview();
+                Console.WriteLine("\nOrder id: " + fo.OrderId);
+                fo.displayTime();
+                fo.Order.payOverview();
             }
-           
+
         }
 
         public void Remove()
@@ -108,13 +109,47 @@ namespace CinemaApp
             List<FoodOrder> userOrders = new List<FoodOrder>();
             foreach (var order in Orders)
             {
-                if (order.UserName == username)
+                if (order.UserName == username && order.Made == false)
                 {
                     userOrders.Add(order);
                 }
             }
 
+            seeCart();
+            bool busy = true;
+            while (busy)
+            {
+                Console.WriteLine("\nEnter the order id or press q to quit...");
+                int userId;
+                string idStr = Console.ReadLine();
+                if (idStr == "q")
+                {
+                    busy = false;
+                    break;
+                }
+                bool sucess = Int32.TryParse(idStr, out userId);
 
+                FoodOrder temp = null;
+                if ((sucess) & (userId <= userOrders.Count) & (userId >= 0))
+                {
+                    Console.WriteLine("You selected: ");
+                    foreach (var fo in userOrders)
+                    {
+                        if (fo.OrderId == userId)
+                        {
+                            temp = fo;
+                            Console.WriteLine("\nOrder id: " + fo.OrderId);
+                            fo.displayTime();
+                            fo.Order.overview();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Order not found");
+                }
+                Orders.Remove(temp);
+            }
         }
 
         public static void Costumer()
@@ -126,10 +161,11 @@ namespace CinemaApp
             string rawJson = File.ReadAllText(fileName);
             List<FoodOrder> all = JsonConvert.DeserializeObject<List<FoodOrder>>(rawJson);
             var allOrders = new CostumerFoodOrder(all);
-            
+
 
             while (busy)
             {
+                TheMenu.overview();
                 Console.WriteLine("\nHi, what do you wanna do?\n1. See menu\n2. Search menu\n3. See what's in your cart\n4. New food order\n5. Checkout\npress q to quit....");
                 var choice = Console.ReadKey().Key;
                 if (choice == ConsoleKey.Q)
@@ -153,7 +189,7 @@ namespace CinemaApp
                 {
                     all.Add(FoodOrder.Add());
                     allOrders = new CostumerFoodOrder(all);
-                    
+
                 }
                 else if (choice == ConsoleKey.D5)
                 {
