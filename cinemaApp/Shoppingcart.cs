b17f474aea0e3d8f3370e59c4274aa75808d1482
TobiZelders <0981403@hr.nl>
2020-06-02 14:19:06 +0200

    Tobias commit

--- cinemaApp/Shoppingcart.cs
+++ cinemaApp/Shoppingcart.cs
@@ -7,49 +7,63 @@ namespace cinemaApp
 {
     public class Shoppingcart
     {
-        public static void ShoppingcartNav()
+        
+        public static void ShoppingcartNav(User user)
         {
             //Eerst current shoppingcart laten zien
-            Console.WriteLine("\nPlease pick a option.\n[1] Checkout.\n[2] Continue shopping.\n[3] Back.");
-            int choice = Program.ChoiceInput(0, 3);
+            Console.WriteLine("\nPlease pick a option.\n[1] Checkout.\n[2] Edit shoppingcart.\n[3] Continue shopping.\n[4] Back.");
+            int choice = Program.ChoiceInput(0, 4);
             while(choice != 0) {
                 if (choice == 1)
                 {
-                    ShoppingcartCheckout();
+                    ShoppingcartCheckout(user);
                 }
                 else if (choice == 2)
                 {
-                    Console.WriteLine("[1] Reserve tickets\n[2] Buy food.");
-                    int choice2 = Program.ChoiceInput(0, 2);
+
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("\nPlease pick a option.\n[1] Reserve tickets\n[2] Buy food.\n[3] Back.");
+                    int choice2 = Program.ChoiceInput(0, 3);
                     while (choice2 != 0)
                     {
                         if (choice2 == 1)
                         {
-                            Console.WriteLine("Reserve tickets.");
-                            Console.ReadLine();
+                            ReserveTickets.ReserveTicketsMain(user);
                         }
                         else if (choice2 == 2)
                         {
                             Console.WriteLine("Buy food.");
-                            Console.ReadLine();
+                            CostumerFoodOrder.Costumer();
+                        }
+                        else if (choice2 == 3)
+                        {
+                            Shoppingcart.ShoppingcartNav(user);
                         }
                     }
                 }
-                else if (choice == 3)
+                else if (choice == 4)
                 {
-                    // terug naar navigation costumer
+                    Navigation.CustomerNavigation(user);
                 }  
             }
         }
         static void ShoppingcartMain()
         {
-            StreamWriter sw = new StreamWriter(@"Shoppingcart.txt", append: true);
+            var ShoppingcartItems = new[] { "Film", "Burger", "Friet", "Water" };
+            foreach (var item in ShoppingcartItems)
+            {
+
+            }
+
         }
         static void ShoppincartShowItems()
         {
+            
             //code connecten aan reservetickets.savetoshoppingcart
         }
-        static void ShoppingcartCheckout()
+        static void ShoppingcartCheckout(User user)
         {
             float totalCheckout = 0f;
             int creditcardMaxNumbers = 5;
@@ -71,7 +85,7 @@ namespace cinemaApp
                 }
             }
             Console.WriteLine("Thank you for your purchase!\nHere is your reservation code: (reservation code)");
-            ShoppingcartNav();
+            ShoppingcartNav(user);
         }
     }
 }
