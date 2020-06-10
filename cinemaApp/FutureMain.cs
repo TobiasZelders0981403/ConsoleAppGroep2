373f60de98617f3955e8c67a7b8e2ed7bfb3ed90
inphamie <58366460+inphamie@users.noreply.github.com>
2020-06-10 13:50:43 +0200

    Creating future filmlist.

---
+++ cinemaApp/FutureMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films2_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FutureFilm.FutureMain();
+            Console.Read();
+        }
+    }
+}
+
+
